0a5ab0e946df4b044cde9e6a8cac232fa7359e1f
Artur Victorovich <mostvad7777@gmail.com>
2026-06-25 18:55:01 +0700

    .gitmessage <feat>(<home>): <ограничил разрешение приложения под мобильные экраны до 390px>
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   src/screens/Home.tsx
    
    Untracked files:
    	../.gitignore

--- pet.proj1/src/screens/Home.tsx
+++ pet.proj1/src/screens/Home.tsx
@@ -4,8 +4,8 @@ import { StartGame } from '@/components/GuessingGame/StartGame';
 
 function Home() {
   return (
-    <div className="relative flex h-dvh w-full flex-col items-center justify-center overflow-hidden">
-      <div className="layout scrollbar-hidden flex h-full min-h-0 w-full flex-col items-center overflow-y-auto bg-app-bg px-2 py-4 min-[390px]:px-4 md:overflow-hidden">
+    <div className="relative flex h-dvh w-full flex-col items-center justify-center overflow-hidden bg-card-bg-secondary ">
+      <div className="layout max-w-[390px] md:max-w-full bg-app-bg scrollbar-hidden flex h-full min-h-0 w-full flex-col items-center overflow-y-auto  px-2 py-4 min-[390px]:px-4 md:overflow-hidden">
         <Header />
 
         <main className="flex min-h-0 w-full flex-1 flex-col">
